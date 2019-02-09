a6c87c8c054315b68baedd889545f15e38f5f40a
Andres Rivera <andres.rivera.toro@gmail.com>
2019-02-09 13:36:59 -0500

    Add POST for /games

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const server = express();
+
+server.post('/games', (req, res) => {
+  const game = {
+    title: 'Pacman', // required
+    genre: 'Arcade', // required
+    releaseYear: 1980 // not required
+  };
+  if (game.title && game.genre) {
+    res.status(200).json(game);
+  } else {
+    res.status(422).json({ error: 'missing required fields' });
+  }
+});
+
+module.exports = server;
